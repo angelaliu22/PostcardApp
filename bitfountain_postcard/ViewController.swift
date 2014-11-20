04f785937ce742c7422cfbb8a3e46f2f062e0680
Angela Liu <angelaliu@Angelas-MacBook-Air.local>
2014-11-20 01:26:15 -0500

    Initial Commit

---
+++ bitfountain_postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitfountain_postcard
+//
+//  Created by Angela Liu on 11/20/14.
+//  Copyright (c) 2014 Angela Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
